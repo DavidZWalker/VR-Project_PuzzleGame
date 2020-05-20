60ebb9283eb05995eaea195b1268830915376097
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2020-05-20 12:23:51 +0200

    added color highlight for objects with >= 1 material

--- Assets/Scripts/InteractableBase.cs
+++ Assets/Scripts/InteractableBase.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public abstract class InteractableBase : MonoBehaviour, IInteractable
 {
     private Outline _outline;
 
+    private Renderer renderer;
+    private int anzahl = 0;
+    Color[] colors;
+
+    public Color HighlightColor = Color.blue;
+
     public void Start()
     {
-        _outline = GetComponent<Outline>();
-        _outline.enabled = false;
+        //_outline = GetComponent<Outline>();
+        //_outline.enabled = false;
         InternalStart();
     }
 
@@ -25,17 +32,48 @@ public abstract class InteractableBase : MonoBehaviour, IInteractable
 
     public void OnPointerEnter()
     {
+        SaveOriginalColors();
         HighlightObject();
+    }
+
+    private void SaveOriginalColors()
+    {
+        renderer = GetComponent<Renderer>();
+        foreach (var m in renderer.materials)
+        {
+            anzahl++;
+        }
+
+        colors = new Color[anzahl];
+        int index = 0;
+
+        foreach (var mat in renderer.materials)
+        {
+            colors[index] = mat.color;
+            index++;
+        }
     }
 
     public void OnPointerExit()
     {
-        _outline.enabled = false;
+        //_outline.enabled = false;
+        renderer = GetComponent<Renderer>();
+        int index = 0;
+        foreach (var mat in renderer.materials)
+        {
+            mat.color = colors[index];
+            index++;
+        }
     }
 
     private void HighlightObject()
     {
-        _outline.enabled = true;
+        //_outline.enabled = true;
+        renderer = GetComponent<Renderer>();
+        foreach (var mat in renderer.materials)
+        {
+            mat.color = HighlightColor;
+        }
     }
     
     
