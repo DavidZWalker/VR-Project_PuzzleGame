486b7babdb07269080b7c9d989b43d0085d22a00
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2020-05-22 18:07:40 +0200

    added cars to the road

---
+++ Assets/Scripts/CarS/CarControllerWithRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControllerWithRotate : MonoBehaviour
+{
+    public float _movementSpeed = 5f;
+    public Vector3 _targetPosition;
+    private Vector3 _currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var timeSinceLastFrame = Time.deltaTime;
+        _currentPosition = transform.position;
+
+        transform.position = Vector3.MoveTowards(_currentPosition, _targetPosition, _movementSpeed * timeSinceLastFrame);
+        
+        if (_currentPosition == _targetPosition)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/CarS/CarSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public float spawnInterval = 5f;
+    public GameObject spawnObject;
+    public GameObject spawnObject2;
+    public GameObject spawnObject3;
+
+    private GameObject nextSpawnedObject;
+
+    private float secondsSinceLastSpawn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNextSpawnObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var secondsSinceLastFrame = Time.deltaTime;
+        secondsSinceLastSpawn += secondsSinceLastFrame;
+
+        if (secondsSinceLastSpawn >= spawnInterval)
+        {
+            secondsSinceLastSpawn = 0;
+            //Spawn Enemy
+            Instantiate(nextSpawnedObject, transform.position, Quaternion.identity);
+            SetNextSpawnObject();
+        }
+
+    }
+
+    void SetNextSpawnObject()
+    {
+        System.Random random = new System.Random();
+        int number = random.Next(3);
+        if (number == 0)
+        {
+            nextSpawnedObject = spawnObject;
+        }
+        if (number == 1)
+        {
+            nextSpawnedObject = spawnObject2;
+        }
+        if (number == 2)
+        {
+            nextSpawnedObject = spawnObject3;
+        }
+    }
+}
